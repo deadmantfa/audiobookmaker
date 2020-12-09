80c85a0ea483bf7e551c3b2de8fc3f968814e104
deadmantfa <wenceslausdsilva@gmail.com>
2020-12-10 00:23:48 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('web.png'))
+
+        self.show()
+
+
+app = QApplication(sys.argv)
+
+ex = Example()
+sys.exit(app.exec_())
